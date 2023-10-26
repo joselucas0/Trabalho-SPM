891b772740298f63023a5a1a4facc3574718db97
José Lucas Rocha <112421411+joselucas0@users.noreply.github.com>
2023-10-26 10:42:59 -0300

    Update registros.h

--- include/registros.h
+++ include/registros.h
@@ -1,1 +1,11 @@
 #define MAX 80
+
+struct policiais {
+    char nome[50];
+    char cpf[12];
+    char nome_de_guerra[20];
+    char cidade[50];
+    int idade;
+    char cargo[30];
+    char senha[20];
+};
